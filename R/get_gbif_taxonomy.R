657586ac2c0437b1c0e00223c2f12546b6e9fed3
swomics <sam.a.whiteford@gmail.com>
2026-01-20 10:17:35 +0000

    Update get_gbif_taxonomy.R

--- R/get_gbif_taxonomy.R
+++ R/get_gbif_taxonomy.R
@@ -140,7 +140,7 @@ get_gbif_taxonomy <- function(x,
     
     # resolve all synonyms, if allowed 
      
-    if(!any(temp[[i]]$status == "ACCEPTED") & any(temp[[i]]$status == "SYNONYM")) {
+    if(!any(temp[[i]]$status == "ACCEPTED") & any(temp[[i]]$status == "SYNONYM" ) & any(temp[[i]]$rank == "species")) {
       if(resolve_synonyms) {
           keep <- temp[i]
           temp[i] <- taxize::get_gbifid_(temp[[i]]$species[which.max(temp[[i]]$confidence)], messages = verbose)
